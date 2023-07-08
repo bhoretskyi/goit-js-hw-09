3d77ca7dd415a0cc3be76e339537e854101ecac3
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-07-08 20:14:38 +0200

    complete_1st_task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body');
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStopClick);
+function onStartClick() {
+  timerID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+}
+function onStopClick() {
+  clearInterval(timerID);
+  startButton.disabled = false;
+}
